700a19c2752b57a87a5208527a85f684bc143082
Aryusend <89976639+Aryusend@users.noreply.github.com>
2023-05-03 01:45:15 +0900

    이미지 추가 및 메인 뷰 배치

--- Res/Project/app/src/main/java/com/spg2018184028/sprinter/MainActivity.java
+++ Res/Project/app/src/main/java/com/spg2018184028/sprinter/MainActivity.java
@@ -3,12 +3,38 @@ package com.spg2018184028.sprinter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private ImageButton leftButton, rightButton, jumpButton, pauseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        leftButton = findViewById(R.id.ButtonLeft);
+        rightButton = findViewById(R.id.ButtonRight);
+        jumpButton = findViewById(R.id.ButtonJump);
+        pauseButton = findViewById(R.id.ButtonPause);
+    }
+
+    public void onClickLeftButton(View view) {
+        Log.d(TAG, "left button clicked!!");
+    }
+
+    public void onClickRightButton(View view) {
+        Log.d(TAG, "right button clicked!!");
+    }
+
+    public void onClickJumpButton(View view) {
+        Log.d(TAG, "jump button clicked!!");
+    }
+
+    public void onClickPauseButton(View view) {
+        Log.d(TAG, "pause button clicked!!");
     }
 }
