c41d9a897a7158f20c61cec60eb2747a58093ef0
Aryusend <89976639+Aryusend@users.noreply.github.com>
2023-05-03 02:16:44 +0900

    게임 뷰 추가 및 메인 배치

---
+++ Res/Project/app/src/main/java/com/spg2018184028/sprinter/GameView.java
@@ -0,0 +1,36 @@
+package com.spg2018184028.sprinter;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+public class GameView extends View {
+
+    public GameView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
+}
